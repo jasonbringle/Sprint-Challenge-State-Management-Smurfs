7850871d9b2f5b967363f2ce82335162d96e72a5
jasonbringle <jasonbringle@gmail.com>
2020-05-31 19:06:03 -0700

    added axios call, form with inputs and now can call DB into console.log

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, } from "react";
 import Header from './Header';
 import Form from './Form';
 import Smurfs from './Smurfs'
-
+import SmurfContext from '../contexts/SmurfContext'
+import axios from 'axios'
 import "./App.css";
 
+
+
 const App = () => {
+  const [ smurfList, setSmurfList] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => setSmurfList(res.data))
+      .catch(err => err)
+  })
   
     return (
-      <div className="App">
+    <div className="App">
         <Header />
+        <SmurfContext.Provider value={{smurfList}}>
         <Form />
         <Smurfs />
+        </SmurfContext.Provider>
       </div>
     );
   
--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState} from 'react'
+import Axios from 'axios'
 
 const Form = () => {
-    return <h1>hello</h1>
+const [ newSmurf, setNewSmurf ] = useState({
+    name: '',
+    age: '',
+    height: '',
+    id: '',
+})
+
+const changeHandler = e => {
+   setNewSmurf({
+    ...newSmurf,
+    [e.target.name]: e.target.value
+    })
+}
+
+const addNewSmurf = e => {
+    e.preventDefault();
+    Axios
+    .post("http://localhost:3333/smurfs", newSmurf)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err.data.error.message))
+}
+
+    return <div className="App">
+       <form onSubmit={addNewSmurf}>
+            <input type="text" onChange={changeHandler} name="name" value={newSmurf.name} placeholder="Smurf Name"/>
+            <input type="text" onChange={changeHandler} name="age" value={newSmurf.age} placeholder="Smurf Age"/>
+            <input type="text" onChange={changeHandler} name="height" value={newSmurf.height} placeholder="Smurf height"/>
+            <button>Add New Smurf</button>
+        </form>
+      </div>
 
 }
 
